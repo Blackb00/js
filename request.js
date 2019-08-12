bf6773d821e11ed8ee2c05984510a157ffb97d28
Dasha F <fda529@mail.ru>
2019-08-13 01:37:31 +0300

    fixed script request.js according to requested changes:

--- request.js
+++ request.js
@@ -1,31 +1,41 @@
-const requestFunc = function (numberOfRequests, requestType) {
-    const makeRequest = function () {
-        var http = require("http");
-        var request = http.request({
-            hostname: "localhost",
-            port: 3000,
-            method: "POST"
-        }, function (response) {
-            response.on("data", function (chunk) {
-                process.stdout.write(chunk.toString());
-            });
+const makeRequest = function () {
+    var http = require("http");
+    var request = http.request({
+        hostname: "localhost",
+        port: 3000,
+        method: "POST"
+    }, function (response) {
+        response.on("data", function (chunk) {
+            process.stdout.write(chunk.toString());
         });
-        request.end("Hello server");
-    }    
-    if (requestType == "async") {
-        const doAsync = async function (func) {
-            await func();
-        }
-        for (let i = 0; i < numberOfRequests; i++) {
-            console.log("request #"+i);
-            doAsync(makeRequest);
-        }
-    } else {
-        for (let i = 0; i < numberOfRequests; i++) {
-            console.log("request #" + i);
-            makeRequest();
-        }
+    });
+    request.end("Hello server");
+}  
+
+
+const requestFunc = function (numberOfRequests) {
+
+    var myArgs = process.argv.slice(2);
+    switch (myArgs[0]) {
+        case "async":
+            const doAsync = async function (func) {
+                await func();
+            }
+            for (let i = 0; i < numberOfRequests; i++) {
+                console.log("request #" + i);
+                doAsync(makeRequest);
+            }
+            break;        
+        case "parallel":
+            for (let i = 0; i < numberOfRequests; i++) {
+                console.log("request #" + i);
+                makeRequest();
+            }
+            break;
+        default:
+            console.log("Can't understand:", myArgs);
+            console.log("Please type 'async' or 'parallel'")
     }
     
 }
-requestFunc(500, "async");
+requestFunc(100);
